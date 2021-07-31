978f82e6a14e5da0d2aafb828d885ccc6e4f355c
Catalina Teguiani <catalinateguiani@yahoo.com>
2021-07-31 15:41:32 +0100

    Create Delete All button

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-let myLeads = [];
+let myLeads=[]
+
+// let myLeads = `["www.hotnews.com"]`;
+
+// myLeads = JSON.stringify(myLeads)
+
+// myLeads = JSON.parse(myLeads) // turn into an array
+
+// myLeads.push("adevarul.ro") // push a new value
+
+// myLeads = JSON.stringify(myLeads) // turn into a string
+
+// console.log(typeof myLeads)
+
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn")//store the delete button in a variable
 
-console.log(localStorage.getItem("myLeads"));
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+
+deleteBtn.addEventListener("dblclick", () => {
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
+
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
   inputEl.value = "";
+
+  // myLeads = JSON.stringify(myLeads)
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads();
 });
+
 
 function renderLeads() {
   let listItems = "";
